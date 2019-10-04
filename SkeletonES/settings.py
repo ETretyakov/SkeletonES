92f14c91f3bc7b3a8ea0eb93177efe30b2bdcc2b
ETretyakov <etretyakov@kaf65.ru>
2019-10-04 14:54:15 +0300

    ALLOWED_HOSTS as a proper list

--- SkeletonES/settings.py
+++ SkeletonES/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [config.get("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = config.get("ALLOWED_HOSTS").split(",")
 print("Allowed hosts:", ALLOWED_HOSTS)
 
 # ElasticSearch
